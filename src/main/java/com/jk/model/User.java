b78844db8afc6c2123f4d1a82f443390f79215f4
changlutong <756335549@qq.com>
2018-05-14 15:40:33 +0800

    地址修改

--- src/main/java/com/jk/model/User.java
+++ src/main/java/com/jk/model/User.java
@@ -14,7 +14,7 @@
  * <author>          <time>          <version>          <desc>
  * 作者姓名           修改时间           版本号              描述
  */
-package com.jk.user;
+package com.jk.model;
 
 import java.io.Serializable;
 
