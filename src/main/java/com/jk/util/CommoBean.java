122aa9216cfbb3aa957f7f24e1cb534b121d4573
yuankang <1056079234@qq.com>
2018-05-21 11:21:35 +0800

    袁康提交的

---
+++ src/main/java/com/jk/util/CommoBean.java
@@ -0,0 +1,30 @@
+package com.jk.util;
+
+public class CommoBean {
+
+    //短信平台地址
+    public static String SMS_URL = "https://api.miaodiyun.com/20150822/industrySMS/sendSMS";
+
+    //短信平台Id
+    public static String ACCOUNTSID = "f5fb27dcac074f72901014ba75dbb6d0";
+
+    //短信模板Id
+    public static String TEMPLATEID = "164840719";
+
+    //手机号
+    public static String tokey = "32f61f78337a4aadbd4139d505cc3df3";
+
+    public static final String WEB_SERVICE_PASSWORD="admin";
+
+    //邮件发送地址
+    public static final String HOST = "smtp.163.com";
+    //端口号
+    public static final Integer PORT = 25;
+    //发送方账号
+    public static final String USERNAME = "yuankang0927@163.com";
+    //发送方密码
+    public static final String PASSWORD = "yuan7464";
+
+    public static final String EMAILFORM = "yuankang0927@163.com";
+
+}
