7c1f50e5f080a0343750a22c68424e950d3905a9
yuankang <1056079234@qq.com>
2018-05-23 11:45:19 +0800

    袁康提交的

--- src/main/java/com/jk/mapper/ICompanyDao.java
+++ src/main/java/com/jk/mapper/ICompanyDao.java
@@ -49,7 +49,7 @@ public interface ICompanyDao {
     long selectCompanyCount(@Param("biaoid") String biaoid);
 
     @Select("select * from ${companytablename} where id=#{phone}")
-    List<Company> querycompanyloginstatus(@Param("phone") String phone,@Param("companytablename") String companytablename);
+    List <Company>querycompanyloginstatus(@Param("phone") String phone,@Param("companytablename") String companytablename);
 
 /*    @Update("update from ${s} set checkstatus=2 where id = #{s1}")
     void updateCompanyStatus(@Param("s1") String s1, @Param("s") String s);*/
